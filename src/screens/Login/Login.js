424172ae1a0ca3750cc2ac1e8637f2422826d0ce
siiddhantt <siddhant.rai.5686@gmail.com>
2023-02-13 02:47:04 +0530

    Login screen improvements

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import { View, ScrollView, Text } from 'react-native';
 import Lottie from 'lottie-react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import { GoogleSigninButton } from '@react-native-google-signin/google-signin';
 import styles from './styles';
 
 export default function Login(props) {
     return (
         <View style={styles.scrollView}>
-            <ScrollView>
-                <View style={styles.container}>
-                    <View style={styles.lottView}>
-                        <Lottie source={require('../../assets/BusAnimation2.json')}
-                            autoPlay loop />
-                    </View>
-                    <View>
-                        <Text style={styles.body}>Welcome to BookMyBus!</Text>
+            <LinearGradient colors={['#cae7fa', '#a1c4fd']} style={styles.lgStyle}>
+                <ScrollView>
+                    <View style={styles.container}>
+                        <View style={styles.lottView}>
+                            <Lottie source={require('../../assets/BusAnimation2.json')}
+                                autoPlay loop />
+                        </View>
+                        <View style={styles.txtView}>
+                            <Text style={styles.bodyTxt}>Welcome to BookMyBus!</Text>
+                        </View>
+                        <View style={styles.txtView}>
+                            <Text style={styles.detailTxt}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.
+                            </Text>
+                        </View>
                     </View>
                     <View style={styles.btnView}>
                         <GoogleSigninButton style={styles.btnStyle}
                             size={GoogleSigninButton.Size.Wide}
+                            color={GoogleSigninButton.Color.Dark}
                             onPress={props.onGoogleButtonPress} />
                     </View>
-                </View>
-            </ScrollView>
+                </ScrollView>
+            </LinearGradient>
         </View>
     );
 };
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -2,11 +2,15 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     scrollView: {
         flex: 1,
-        backgroundColor: "#FFFFFF",
-        width: "100%",
+        backgroundColor: '#FFFFFF',
+        width: '100%',
+    },
+    lgStyle: {
+        height: '100%'
     },
     container: {
         marginTop: 140,
+        marginBottom: 20,
         marginHorizontal: 20
     },
     heading: {
@@ -19,12 +23,31 @@ const styles = StyleSheet.create({
         marginHorizontal: 20,
         padding: 100
     },
+    txtView: {
+        marginHorizontal: 10
+    },
+    bodyTxt: {
+        textAlign: 'center',
+        color: '#000000',
+        fontFamily: 'Poppins-Bold',
+        fontSize: 20
+    },
+    detailTxt: {
+        marginTop: 2,
+        marginHorizontal: '5%',
+        textAlign: 'center',
+        color: '#000000',
+        fontFamily: 'Poppins-Light',
+        fontSize: 15
+    },
     btnView: {
-        marginTop: 20,
+        marginBottom: 10,
         marginHorizontal: 20
     },
     btnStyle: {
-        alignSelf: 'center'
+        alignSelf: 'center',
+        width: 252,
+        height: 48
     }
 });
 
